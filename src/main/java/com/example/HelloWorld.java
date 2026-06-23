976e5f680287aea19b8a72aa48617b65f47ce3ec
rajesh-patil1 <115767329+rajesh-patil1@users.noreply.github.com>
2026-06-23 19:38:39 +0530

    Update HelloWorld.java
    HELLO WORLD

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("it's working ");
+        System.out.println("HELLO WORLD");
     }
 }
 
